d97db9abd71ac35bac9b66ef949446b2b0f20071
wizurek <wizurek@student.wszib.edu.pl>
2020-04-12 00:04:58 +0200

    Pierwszy commit

---
+++ src/main/java/pl/edu/wszib/jwd/homebudgetcontroller/HomeBudgetControllerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.homebudgetcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudgetControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBudgetControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/jwd/homebudgetcontroller/HomeBudgetControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.homebudgetcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBudgetControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
